106474e44a7a366fbc3455f841b0b2d7cbf7d049
alufer8673 <alufer8673@ieselcaminas.org>
2023-12-15 00:06:27 +0100

    ejer17.php sin acabar

--- ejer17.php
+++ ejer17.php
@@ -1,3 +1,18 @@
-<?php
+<html>
+    <body>
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST"){
+                $nombre = $_REQUEST['nombre'];
+                $edad = $_REQUEST['edad'];
 
-?>
+                $mensaje = "";
+                if ($edad >= 18){
+                    $mensaje = "$nombre es mayor de edad";
+                } else {
+                    $mensaje = "$nombre es menor de edad";
+                }
+            }
+// llevo 15 min
+        ?>
+    </body>
+</html>
