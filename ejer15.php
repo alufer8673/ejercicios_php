1f3c7764bae6e6890c57ca908d345afaae02393a
alufer8673 <alufer8673@ieselcaminas.org>
2023-12-18 11:23:04 +0100

    2023_12_15

---
+++ ejer15.php
@@ -0,0 +1,14 @@
+<?php
+    echo "<table border=1>";
+    for ($i = 1; $i <= 4; $i++){
+        echo "<tr>";
+        for ($j = 1; $j <= 4; $j++){
+            echo "<td>";
+            $potencia = pow($i,$j);
+            echo $potencia;
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>"
+?>
